55147319cb98cb1a8ad88a29393300167d4039eb
sharan22544 <neelegarmn@gmail.com>
2025-09-06 22:29:39 +0530

    Create assesment2.py

---
+++ assesment2.py
@@ -0,0 +1,11 @@
+n=int(input("enter a number:"))
+if n%2==0:
+    print(n,"is an even number")
+else:
+    print(n,"is an odd number")
+
+
+a=0
+for i in range(51):
+    a=a+i
+print("the sum of numbers from 1 to 50 is: ",a)
